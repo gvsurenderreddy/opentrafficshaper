d5b4706e65799dd74846eadb371e69665defb131
Nigel Kukard <nkukard@nigel-pc.local>
2013-08-18 18:34:20 +0000

    Added HybridHTTP POE filter

---
+++ opentrafficshaper/POE/Filter/HybridHTTP.pm
@@ -0,0 +1,479 @@
+# POE::Filter::HybridHTTP - Copyright 2013, AllworldIT
+# Hybrid HTTP filter supporting websockets too.
+
+##
+# Code originally based on POE::Filter::HTTPD
+##
+# Filter::HTTPD Copyright 1998 Artur Bergman <artur@vogon.se>.
+# Thanks go to Gisle Aas for his excellent HTTP::Daemon.	Some of the
+# get code was copied out if, unfortunately HTTP::Daemon is not easily
+# subclassed for POE because of the blocking nature.
+
+# 2001-07-27 RCC: This filter will not support the newer get_one()
+# interface.	It gets single things by default, and it does not
+# support filter switching.	If someone absolutely needs to switch to
+# and from HTTPD filters, they should submit their request as a patch.
+##
+
+package POE::Filter::HybridHTTP;
+
+use warnings;
+use strict;
+
+use bytes;
+
+use POE::Filter;
+use POE::Filter::HybridHTTP::WebSocketFrame;
+
+use vars qw($VERSION @ISA);
+# NOTE - Should be #.### (three decimal places)
+$VERSION = '1.000';
+@ISA = qw(POE::Filter);
+
+
+# States of the protocol
+use constant {
+	CRLF => "\r\n",
+	# Protocol states
+	ST_HTTP_HEADERS => 1, # Busy processing headers
+	ST_HTTP_CONTENT => 2, # Busy processing the body
+	ST_WEBSOCKET_STREAM => 3,
+};
+
+use Digest::SHA qw( sha1_base64 );
+use HTTP::Date qw(time2str);
+use HTTP::Request;
+use HTTP::Response;
+use HTTP::Status qw( :constants :is );
+use URI;
+
+my $HTTP_1_0 = _http_version("HTTP/1.0");
+my $HTTP_1_1 = _http_version("HTTP/1.1");
+
+
+# Class instantiation
+sub new 
+{
+	my $class = shift;
+
+	 # These are our internal properties
+	my $self = { };
+	# Build our class
+	bless($self, $class);
+
+	# And initialize
+	$self->_reset();
+
+	return $self;
+}
+
+
+# From the docs:
+# get_one_start() accepts an array reference containing unprocessed stream chunks. The chunks are added to the filter's Internal
+# buffer for parsing by get_one().
+sub get_one_start 
+{
+	my ($self, $stream) = @_;
+
+
+	# Join all the blocks of data and add to our buffer
+	$self->{'buffer'} .= join('',@{$stream});
+
+	return $self;
+}
+
+
+# This is called to see if we can grab records/items
+sub get_one 
+{
+	my $self = shift;
+
+
+	# Waiting for a complete suite of headers.
+	if ($self->{'state'} == ST_HTTP_HEADERS) {
+		return $self->_get_one_http_headers();
+
+	# Waiting for content.
+	} elsif ($self->{'state'} == ST_HTTP_CONTENT) {
+		return $self->_get_one_http_headers();
+		
+	# Websocket
+	} elsif ($self->{'state'} == ST_WEBSOCKET_STREAM) {
+		return $self->_get_one_websocket_record();
+
+	# XXX - better handling?
+	} else {
+		die "Unknown state '".unpack("H*",$self->{'state'})."'";
+	}
+}
+
+
+# Function to push data to the socket
+sub put 
+{
+	my ($self, $responses) = @_;
+	my @results;
+
+
+	# Handle HTTP content
+	if ($self->{'state'} == ST_HTTP_CONTENT || $self->{'state'} == ST_HTTP_HEADERS) {
+		# Compile our list of results
+		foreach my $response (@{$responses}) {
+			# Check if we have a upgrade header
+			if ((my $h_upgrade = $response->header("Upgrade")) && $response->code == HTTP_SWITCHING_PROTOCOLS) {
+				# Check if its a websocket upgrade
+				if (
+						# Is it a request and do we have a original request?
+						$h_upgrade eq "websocket" && defined($self->{'last_request'}) && 
+						# If so was there a websocket-key?
+						(my $websocketKey = $self->{'last_request'}->header('Sec-WebSocket-Key'))
+				) {
+					$self->{'state'} = ST_WEBSOCKET_STREAM;
+					# GUID for this protocol as per RFC6455 Section 1.3
+					my $websocketKeyResponseRaw = $websocketKey."258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+					my $websocketKeyResponse = sha1_base64($websocketKeyResponseRaw);
+					# Pad up to base64 length   4[N/3] 
+					$websocketKeyResponse .= "=" x ((length($websocketKeyResponse) * 3) % 4);
+					$response->push_header('Sec-WebSocket-Accept',$websocketKeyResponse);
+				}
+			}
+
+			push(@results,$self->_build_raw_response($response));
+		}
+
+
+	# Handle WebSocket data
+	} elsif ($self->{'state'} == ST_WEBSOCKET_STREAM) {
+		# Compile our list of results
+		foreach my $response (@{$responses}) {
+			# If we don't have a websocket write state, create one
+			if (!$self->{'state_websocket_write'}) {
+				$self->{'state_websocket_write'} = new POE::Filter::HybridHTTP::WebSocketFrame();
+			}
+			$self->{'state_websocket_write'}->append($response);
+
+			push(@results,$self->{'state_websocket_write'}->to_bytes());
+		}
+	}
+
+	return \@results;
+}
+
+
+
+#
+# Internal functions
+#
+
+# Prepare for next request
+sub _reset
+{
+	my $self = shift;
+
+
+	# Reset our filter state
+	$self->{'buffer'} = '';
+	$self->{'state'} = ST_HTTP_HEADERS;
+	$self->{'state_websocket_read'} = undef;
+	$self->{'state_websocket_write'} = undef;
+	$self->{'last_request'} = $self->{'request'};
+	$self->{'request'} = undef; # We want the last request always
+	$self->{'content_len'} = 0;
+	$self->{'content_added'} = 0;
+}
+
+
+# Internal function to parse an HTTP status line and return the HTTP
+# protocol version.
+sub _http_version 
+{
+	my $version = shift;
+
+	# Rip off the version string
+	if ($version =~ m,^(?:HTTP/)?(\d+)\.(\d+)$,i) {
+		my $nversion = $1 * 1000 + $2;
+		# Return a numerical version of it
+		return $nversion;
+	} else {
+		# Or 0 if we did not match
+		return 0;
+	}
+}
+
+
+# Function to handle HTTP headers
+sub _get_one_http_headers
+{
+	my $self = shift;
+
+
+	# Strip leading whitespace.
+	$self->{'buffer'} =~ s/^\s+//;
+
+	# If we've not found the HTTP headers, just return a blank arrayref
+	if ($self->{'buffer'} !~ s/^(\S.*?(?:\r?\n){2})//s) {
+		return [ ];
+	}
+	# Pull the headers as a string off the buffer 
+	my $header_str = $1;
+
+	# Parse the request line.
+	if ($header_str !~ s/^(\w+)[ \t]+(\S+)(?:[ \t]+(HTTP\/\d+\.\d+))?[^\n]*\n//) {
+		return [
+			$self->_build_error(HTTP_BAD_REQUEST, "Request line parse failure. ($header_str)")
+		];
+	}
+	# Create an HTTP::Request object from values in the request line.
+	my ($method, $uri, $proto) = ($1, $2, $3);
+	# Make sure proto is set
+	if (!_http_version($proto)) {
+		$proto = "HTTP/1.0";
+	}
+	$self->{'protocol'} = $proto;
+	$proto = _http_version($proto);
+
+	# Fix a double starting slash on the path.	It happens.
+	$uri =~ s!^//+!/!;
+
+	# Build our request object
+	my $request = HTTP::Request->new($method, URI->new($uri));
+	# Set protocol
+	$request->protocol($self->{'protocol'});
+
+	# Parse headers.
+	my ($key, $val);
+	HEADER: while ($header_str =~ s/^([^\012]*)\012//) {
+		my $header = $1;
+		$header =~ s/\015$//;
+		if ($header =~ /^([\w\-~]+)\s*:\s*(.*)/) {
+			# If we had a key, it means we must save this key/value pair
+			if ($key) {
+				$request->push_header($key, $val);
+			}
+			# Assign key and value pair from above regex
+			($key, $val) = ($1, $2);
+		# Multi-line header value
+		} elsif ($header =~ /^\s+(.*)/) {
+			$val .= " $1";
+		# We no longer matching, so this is the last header?
+		} else {
+			last HEADER;
+	 	}
+	}
+	# Push on the last header if we had one...
+	$request->push_header($key, $val) if $key;
+
+	# We got a full set of headers.	Fall through to content if we
+	# have a content length.
+	my $content_length = $request->content_length();
+	if(defined($content_length)) {
+		$content_length =~ s/\D//g;
+		# If its invalid, it will be 0 anyway
+		$content_length = int($content_length);
+	}
+	my $content_encoding = $request->content_encoding();
+		
+	# The presence of a message-body in a request is signaled by the
+	# inclusion of a Content-Length or Transfer-Encoding header field in
+	# the request's message-headers. A message-body MUST NOT be included in
+	# a request if the specification of the request method (section 5.1.1)
+	# does not allow sending an entity-body in requests. A server SHOULD
+	# read and forward a message-body on any request; if the request method
+	# does not include defined semantics for an entity-body, then the
+	# message-body SHOULD be ignored when handling the request.
+	# - RFC2616
+	if (!defined($content_length) || !defined($content_encoding)) {
+		$self->{'request'} = $request;
+		$self->_reset();
+		return [ $request ];
+	}
+	
+	# PG- GET shouldn't have a body. But RFC2616 talks about Content-Length
+	# for HEAD.	And My reading of RFC2616 is that HEAD is the same as GET.
+	# So logically, GET can have a body.	And RFC2616 says we SHOULD ignore
+	# it.
+	#
+	# What's more, in apache 1.3.28, a body on a GET or HEAD is read
+	# and discarded.	See ap_discard_request_body() in http_protocol.c and
+	# default_handler() in http_core.c
+	#
+	# Neither Firefox 2.0 nor Lynx 2.8.5 set Content-Length on a GET
+
+	# For compatibility with HTTP/1.0 applications, HTTP/1.1 requests
+	# containing a message-body MUST include a valid Content-Length header
+	# field unless the server is known to be HTTP/1.1 compliant. If a
+	# request contains a message-body and a Content-Length is not given,
+	# the server SHOULD respond with 400 (bad request) if it cannot
+	# determine the length of the message, or with 411 (length required) if
+	# it wishes to insist on receiving a valid Content-Length.
+	# - RFC2616 
+
+	# PG- This seems to imply that we can either detect the length (but how
+	# would one do that?) or require a Content-Length header.	We do the
+	# latter.
+	# 
+	# PG- Dispite all the above, I'm not fully sure this implements RFC2616
+	# properly.	There's something about transfer-coding that I don't fully
+	# understand.
+
+	if (!$content_length) {			
+		# assume a Content-Length of 0 is valid pre 1.1
+		if ($proto >= $HTTP_1_1 && !defined($content_length)) {
+			# We have Content-Encoding, but not Content-Length.
+			$request = $self->_build_error(HTTP_LENGTH_REQUIRED,"No content length found.",$request);
+		}
+		$self->_reset();
+		return [ $request ];
+	}
+
+	$self->{'content_length'} = $content_length;
+	$self->{'state'} = ST_HTTP_CONTENT;
+	$self->{'request'} = $request; 
+
+	return [ ];
+}
+
+
+sub _get_one_http_content
+{
+	my $self = shift;
+
+
+	my $request = $self->{'request'};
+	my $content_needed = $self->{'content_length'} - $self->{'content_added'};
+	if ($content_needed < 1) {
+		# We somehow got too much content
+		$request = $self->_build_error(HTTP_BAD_REQUEST, "Too much content received");
+		$self->_reset();
+		return [ $request ];
+	}
+
+	# Not enough content to complete the request. Add it to the
+	# request content, and return an incomplete status.
+	if ((my $buflen = length($self->{'buffer'})) < $content_needed) {
+		$request->add_content($self->{'buffer'});
+		$self->{'content_added'} += $buflen;
+		$self->{'buffer'} = '';
+		return [ ];
+	}
+
+	# Enough data.	Add it to the request content.
+	# PG- CGI.pm only reads Content-Length: bytes from STDIN.
+
+	# Four-argument substr() would be ideal here, but it's not
+	# entirely backward compatible.
+	$request->add_content(substr($self->{'buffer'}, 0, $content_needed));
+	substr($self->{'buffer'}, 0, $content_needed) = "";
+
+	# Some browsers (like MSIE 5.01) send extra CRLFs after the
+	# content.	Shame on them.
+	$self->{'buffer'} =~ s/^\s+//;
+
+	# XXX Should we throw the body away on a GET or HEAD? Probably not.
+
+	# XXX Should we parse Multipart Types bodies?
+
+	# Prepare for the next request, and return this one.
+	$self->_reset();
+
+	return [ $request ];
+}
+
+
+# Function to get a websocket record set
+sub _get_one_websocket_record
+{
+	my $self = shift;
+
+
+	# If we don't have a websocket state, create one
+	if (!$self->{'state_websocket_read'}) {
+		$self->{'state_websocket_read'} = new POE::Filter::HybridHTTP::WebSocketFrame();
+	}
+	$self->{'state_websocket_read'}->append($self->{'buffer'});
+	# Blank our buffer
+	$self->{'buffer'} = '';
+
+	# Loop with records and push onto result set
+	my @results;
+	while (my $item = $self->{'state_websocket_read'}->next()) {
+		push(@results,$item);
+	}
+
+	return \@results;
+}
+
+
+# Build a basic error to return
+sub _build_error
+{
+	my($self, $status, $details, $request) = @_;
+
+
+	# Setup defaults
+	if (!defined($status)) {
+		$status	= HTTP_BAD_REQUEST;
+	}
+	if (!defined($details)) {
+		$details = '';
+	}
+	my $message = "Unknown Error";
+	if (my $msg = status_message($status)) {
+		$message = $msg;
+	}
+
+	# Build the response object
+	my $response = new HTTP::Response->new($status,$message);
+	$response->content(<<EOF);
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>$status $message</title>
+	</head>
+
+	<body>
+		<h1>$message</h1>
+		<p>$details</p>
+	</body>
+</html>
+EOF
+
+	# If we have a request set it
+	if ($request) {
+		$response->request($request);
+	}
+
+	return $response;
+}
+
+
+# Build a socket friendly response
+sub _build_raw_response
+{
+	my ($self,$response) = @_;
+
+
+	# Check for headers we should return
+	if (!defined($response->header("Date"))) {
+		$response->push_header("Date",time2str(time));
+	}
+	if (!defined($response->header("Server"))) {
+		$response->push_header("Server","POE Hybrid HTTP Server v$VERSION");
+	}
+	# Set our content Length
+	if (my $length = length($response->content)) {
+	    $response->push_header("Content-Length",$length);
+	}
+
+	# Setup our output
+	my $output = sprintf("%s %s",$self->{'protocol'},$response->status_line);
+	$output .= CRLF;
+	$output .= $response->headers_as_string(CRLF);
+	$output .= CRLF;
+	$output .= $response->content;
+
+	return $output;
+}
+
+
+1;
---
+++ opentrafficshaper/POE/Filter/HybridHTTP/WebSocketFrame.pm
@@ -0,0 +1,316 @@
+# POE::Filter::HybridHTTP::WebSocketFrame - Copyright 2013, AllworldIT
+# Hybrid HTTP filter support for WebSocketFrames
+
+##
+# Code originally based on Protocol::WebSocket::Frame
+##
+# CREDITS:
+# Paul "LeoNerd" Evans
+# Jon Gentle
+# Lee Aylward
+# Chia-liang Kao
+# Atomer Ju
+# Chuck Bredestege
+# Matthew Lien (BlueT)
+# AUTHOR:
+# Viacheslav Tykhanovskyi, C<vti@cpan.org>.
+# COPYRIGHT:
+# Copyright (C) 2010-2012, Viacheslav Tykhanovskyi.
+# This program is free software, you can redistribute it and/or modify it under
+# the same terms as Perl 5.10.
+##
+
+package POE::Filter::HybridHTTP::WebSocketFrame;
+
+use strict;
+use warnings;
+
+use Config;
+use Encode ();
+
+# Random number generation
+use constant MAX_RAND_INT => 2 ** 32;
+use constant MATH_RANDOM_SECURE => eval "require Math::Random::Secure;";
+
+our %TYPES = (
+	text => 0x01,
+	binary => 0x02,
+	ping => 0x09,
+	pong => 0x0a,
+	close => 0x08
+);
+
+use constant {
+	WS_MAX_FRAGMENTS => 1024,
+	WS_MAX_PAYLOAD_SIZE => 131072,
+};
+
+
+sub new {
+	my ($class,$buffer) = @_;
+
+
+	if (my $classRef = ref $class) {
+		$class = $classRef;
+	}
+
+	# If we don't have a buffer just use an empty string
+	if (!defined($buffer)) {
+		$buffer = "";
+	}
+
+	# Setup ourself
+	my $self = {
+		'buffer' => Encode::is_utf8($buffer) ? Encode::encode('UTF-8', $buffer) : $buffer,
+		'fragments' => [ ],
+		'max_payload_size' => WS_MAX_PAYLOAD_SIZE,
+		'max_fragments' => WS_MAX_FRAGMENTS,
+	};
+
+	bless($self,$class);
+
+	return $self;
+}
+
+
+sub append {
+	my ($self,$data) = @_;
+
+	# If there is no data just return
+	if (!defined($data)) {
+		return;
+	}
+
+	$self->{'buffer'} .= $data;
+
+	return $self;
+}
+
+
+sub next {
+	my $self = shift;
+
+	# If we have next_bytes return
+	if (defined(my $bytes = $self->next_bytes)) {
+		return Encode::decode('UTF-8', $bytes);
+	}
+
+	return; 
+}
+
+sub fin	{ @_ > 1 ? $_[0]->{fin}	= $_[1] : $_[0]->{fin} }
+sub rsv	{ @_ > 1 ? $_[0]->{rsv}	= $_[1] : $_[0]->{rsv} }
+sub opcode { @_ > 1 ? $_[0]->{opcode} = $_[1] : $_[0]->{opcode} || 1 }
+sub masked { @_ > 1 ? $_[0]->{masked} = $_[1] : $_[0]->{masked} }
+
+sub is_ping   { $_[0]->opcode == 9 }
+sub is_pong   { $_[0]->opcode == 10 }
+sub is_close  { $_[0]->opcode == 8 }
+sub is_text   { $_[0]->opcode == 1 }
+sub is_binary { $_[0]->opcode == 2 }
+
+
+sub next_bytes {
+	my $self = shift;
+
+	return unless length $self->{'buffer'} >= 2;
+
+	while (my $buffer_len = length($self->{'buffer'})) {
+		my $offset = 0;
+
+
+		# Grab first byte
+		my $hdr = substr($self->{'buffer'}, $offset, 1);
+		# Reduce first hdr byte to bits
+		my @bits = split //, unpack("B*", $hdr);
+		# Set the FIN attribute
+		$self->fin($bits[0]);
+		# And the RSV (reserved)
+		$self->rsv([@bits[1 .. 3]]);
+
+		# Pull off the opcode & update offset
+		my $opcode = unpack('C', $hdr) & 0b00001111;
+		$offset += 1;	# FIN,RSV[1-3],OPCODE
+
+		# Grab payload length
+		my $payload_len = unpack 'C', substr($self->{'buffer'}, $offset, 1);
+
+		# Check if the payload is masked, if it is flag it internally
+		my $masked = ($payload_len & 0b10000000) >> 7;
+		$self->masked($masked);
+		$offset += 1;	  # + MASKED,PAYLOAD_LEN
+
+		$payload_len = $payload_len & 0b01111111;
+		if ($payload_len == 126) {
+			# Not enough data
+			if ($buffer_len < $offset + 2) {
+				return;
+			}
+			# Unpack the payload_len into its actual value & bump the offset
+			$payload_len = unpack 'n', substr($self->{'buffer'}, $offset, 2);
+			$offset += 2;
+
+		} elsif ($payload_len > 126) {
+			# Not enough data
+			if ($buffer_len < $offset + 4) {
+				return;
+			}
+
+			# Map off the first 8 bits
+			my $bits = unpack('B*',substr($self->{'buffer'},$offset,8));
+			# Most significant bit must be 0
+			substr($bits,0,1,0);
+
+			# Can we not handle 64bit numbers?
+			if ($Config{'ivsize'} <= 4 || $Config{longsize} < 8) {
+				# If not, just use 32 bits
+				$bits = substr($bits, 32);
+				$payload_len = unpack('N',pack('B*',$bits));
+			# If we can use everything we have
+			} else {
+				$payload_len = unpack('Q>',pack('B*',$bits));
+			}
+			# Bump offset
+			$offset += 8;
+		}
+		# XXX - not sure how to return this sanely
+        if ($payload_len > $self->{'max_payload_size'}) {
+			$self->{'buffer'} = '';
+			return;
+		}
+
+		# Grab the mask
+		my $mask;
+		if ($self->masked) {
+			# Not enough data
+			if ($buffer_len < $offset + 4) {
+				return;
+			}
+			# Pull it off
+			$mask = substr($self->{'buffer'}, $offset, 4);
+			$offset += 4;
+		}
+
+		# Check if we have enough data to satisfy our payload_len
+		if ($buffer_len < $offset + $payload_len) {
+			return;
+		}
+
+		# If we do, rip it all off and shove it into $payload
+		my $payload = substr($self->{'buffer'}, $offset, $payload_len);
+
+		# If our data is masked, unmask it
+		if ($self->masked) {
+			$payload = $self->_mask($payload, $mask);
+		}
+
+		substr($self->{'buffer'}, 0, $offset + $payload_len, '');
+
+		# Injected control frame
+		if (@{$self->{'fragments'}} && $opcode & 0b1000) {
+			$self->opcode($opcode);
+			return $payload;
+		}
+
+		# Check if this is the last packet in a set of fragments, if it is combine everything
+		if ($self->fin) {
+			if (@{$self->{'fragments'}}) {
+				$self->opcode(shift @{$self->{'fragments'}});
+			} else {
+				$self->opcode($opcode);
+			}
+			# Join everything up
+			$payload = join('',@{$self->{'fragments'}},$payload);
+			$self->{'fragments'} = [];
+			# And return
+			return $payload;
+
+		} else {
+			# Remember first fragment opcode
+			if (!@{$self->{'fragments'}}) {
+				push @{$self->{'fragments'}}, $opcode;
+			}
+
+			push(@{$self->{'fragments'}},$payload);
+
+			# XXX - Handle sanely?
+			if (@{$self->{'fragments'}} > $self->{'max_fragments'}) {
+				$self->{'fragments'} = [];
+				$self->{'buffer'} = '';
+				return;
+			}
+		}
+	}
+
+	return;
+}
+
+sub to_bytes {
+	my $self = shift;
+
+
+	my $string = '';
+
+	my $opcode;
+	if (my $type = $self->{'type'}) {
+		$opcode = $TYPES{$type};
+	}
+	else {
+		$opcode = $self->opcode || 1;
+	}
+
+	$string .= pack 'C', ($opcode + 128);
+
+	my $payload_len = length($self->{'buffer'});
+	if ($payload_len <= 125) {
+		$payload_len |= 0b10000000 if $self->masked;
+		$string .= pack 'C', $payload_len;
+	}
+	elsif ($payload_len <= 0xffff) {
+		$string .= pack 'C', 126 + ($self->masked ? 128 : 0);
+		$string .= pack 'n', $payload_len;
+	}
+	else {
+		$string .= pack 'C', 127 + ($self->masked ? 128 : 0);
+
+		# Shifting by an amount >= to the system wordsize is undefined
+		$string .= pack 'N', $Config{'ivsize'} <= 4 ? 0 : $payload_len >> 32;
+		$string .= pack 'N', ($payload_len & 0xffffffff);
+	}
+
+	if ($self->masked) {
+
+		my $mask = $self->{mask}
+		  || (
+			MATH_RANDOM_SECURE
+			? Math::Random::Secure::irand(MAX_RAND_INT)
+			: int(rand(MAX_RAND_INT))
+		  );
+
+		$mask = pack 'N', $mask;
+
+		$string .= $mask;
+		$string .= $self->_mask($self->{'buffer'}, $mask);
+	}
+	else {
+		$string .= $self->{'buffer'};
+	}
+
+	# Wipe buffer
+	$self->{'buffer'} = '';
+
+	return $string;
+}
+
+sub _mask {
+	my $self = shift;
+	my ($payload, $mask) = @_;
+
+	$mask = $mask x (int(length($payload) / 4) + 1);
+	$mask = substr($mask, 0, length($payload));
+	$payload ^= $mask;
+
+	return $payload;
+}
+
+1;
