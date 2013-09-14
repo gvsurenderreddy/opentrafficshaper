4743acb63bbf551040430547a498dc850d1c3457
Nigel Kukard <nkukard@lbsd.net>
2013-09-14 17:50:07 +0000

    Refactored code, better asthetics

--- opentrafficshaper/plugins/webserver/pages/limits.pm
+++ opentrafficshaper/plugins/webserver/pages/limits.pm
@@ -34,19 +34,21 @@ use DateTime;
 use HTML::Entities;
 use HTTP::Status qw( :constants );
 use URI::Escape;
+use URI::QueryParam;
 
 use opentrafficshaper::logger;
 use opentrafficshaper::plugins;
-use opentrafficshaper::utils;
+use opentrafficshaper::utils qw( parseURIQuery parseFormContent isUsername isIP isNumber prettyUndef );
 
-use opentrafficshaper::plugins::configmanager qw( getLimits getTrafficClasses getPriorityName );
+use opentrafficshaper::plugins::configmanager qw( getLimits getLimit getTrafficClasses getTrafficClassName isTrafficClassValid );
 
 
 
 # Sidebar menu options for this module
 my $menu = {
-	'Limits' =>  {
-		'Show Limits' => '',
+	'View Limits' =>  {
+		'All Limits' => '',
+		'Manual Limits' => './?source=plugin.webserver.limits',
 	},
 	'Admin' => {
 		'Add Limit' => 'add',
@@ -61,7 +63,10 @@ sub default
 	my ($kernel,$globals,$client_session_id,$request) = @_;
 
 
-	my $limits = getLimits();
+	my @limits = getLimits();
+
+	# Pull in URL params
+	my $queryParams = parseURIQuery($request);
 
 	# Build content
 	my $content = "";
@@ -77,37 +82,47 @@ sub default
 			<th>IP</th>
 			<th>Class</th>
 			<th>Group</th>
-			<th>Limits</th>
+			<th>CIR (Kbps)</th>
+			<th>Limits (Kbps)</th>
 			<th></th>
 		</tr>
 	</thead>
 	<tbody>
 EOF
 	# Body
-	foreach my $lid (getLimits()) {
+	foreach my $lid (@limits) {
 		my $limit;
 		# If we can't get the limit just move onto the next
 		if (!defined($limit = getLimit($lid))) {
 			next;
 		}
 
+		# Conditionals
+		if (defined($queryParams->{'source'})) {
+			if ($limit->{'Source'} ne $queryParams->{'source'}) {
+				next;
+			}
+		}
+
 		# Make style a bit pretty
 		my $style = "";
 		my $icon = "";
 		if ($limit->{'Status'} eq "offline") {
-			$icon = '<i class="glyphicon-trash"></i>';
+			$icon = '<span class="glyphicon glyphicon-trash"></span>';
 			$style = "warning";
 		} elsif ($limit->{'Status'} eq "new") {
 #			$icon = '<i class="glyphicon-plus"></i>';
 			$style = "info";
 		} elsif ($limit->{'Status'} eq "conflict") {
-			$icon = '<i class="glyphicon-random"></i>';
+			$icon = '<span class="glyphicon glyphicon-random"></span>';
 			$style = "error";
 		}
 
 		# Get a nice last update string
 		my $lastUpdate = DateTime->from_epoch( epoch => $limit->{'LastUpdate'} )->iso8601();
-		my $limits = $limit->{'TrafficLimitTx'} . "/" . $limit->{'TrafficLimitRx'};
+
+		my $cirStr = sprintf('%s/%s',prettyUndef($limit->{'TrafficLimitTx'}),prettyUndef($limit->{'TrafficLimitRx'}));
+		my $limitStr = sprintf('%s/%s',prettyUndef($limit->{'TrafficLimitTxBurst'}),prettyUndef($limit->{'TrafficLimitRxBurst'}));
 
 
 		# If the statistics plugin is loaded pull in some stats
@@ -119,19 +134,20 @@ EOF
 			if (my $statsTx = $stats->{'tx'}) {
 				$statsPPSTx = $statsTx->{'current_pps'};
 				$statsRateTx = $statsTx->{'current_rate'};
-				$statsPrioTx = getPriorityName($limit->{'TrafficPriority'});
+				$statsPrioTx = $statsTx->{'priority'};
 			}
 			# Pull off rx stats
 			if (my $statsRx = $stats->{'rx'}) {
 				$statsPPSRx = $statsRx->{'current_pps'};
 				$statsRateRx = $statsRx->{'current_rate'};
-				$statsPrioRx = getPriorityName($limit->{'TrafficPriority'});
+				$statsPrioRx = $statsRx->{'priority'};
 			}
 		}
 
 		my $usernameEncoded = encode_entities($limit->{'Username'});
 		my $usernameEscaped = uri_escape($limit->{'Username'});
 
+		my $classStr = getTrafficClassName($limit->{'ClassID'});
 
 		$content .= <<EOF;
 		<tr class="$style">
@@ -172,19 +188,21 @@ EOF
 				</span>
 			</td>
 			<td>$limit->{'IP'}</td>
-			<td>$limit->{'ClassID'}</td>
+			<td>$classStr</td>
 			<td>$limit->{'GroupID'}</td>
-			<td>$limits</td>
+			<td>$cirStr</td>
+			<td>$limitStr</td>
 			<td>
-				<a href="/statistics/by-username?username=$usernameEscaped"><i class="glyphicon glyphicon-stats"></i></a>
-				<i class="glyphicon glyphicon-wrench"></i>
+				<a href="/statistics/by-username?username=$usernameEscaped"><span class="glyphicon glyphicon-stats"></span></a>
+				<a href="/limits/limit-edit?username=$usernameEscaped"><span class="glyphicon glyphicon-wrench"></span></a>
+				<a href="/limits/limit-remove?username=$usernameEscaped"><span class="glyphicon glyphicon-remove"></span></a>
 			</td>
 		</tr>
 EOF
 	}
 
 	# No results
-	if (keys %{$limits} < 1) {
+	if (!@limits) {
 		$content .=<<EOF;
 		<tr class="info">
 			<td colspan="8"><p class="text-center">No Results</p></td>
@@ -241,13 +259,17 @@ sub add
 	my @errors;
 	# Form items
 	my $params = {
+		'inputFriendlyName' => undef,
 		'inputUsername' => undef,
 		'inputIP' => undef,
 		'inputTrafficClass' => undef,
+		'inputLimitTx' => undef,
+		'inputLimitTxBurst' => undef,
+		'inputLimitRx' => undef,
+		'inputLimitRxBurst' => undef,
 		'inputExpires' => undef,
 		'inputExpiresModifier' => undef,
-		'inputLimitTx' => undef,
-		'inputLimitRx' => undef,
+		'inputNotes' => undef,
 	};
 
 	# If this is a form try parse it
@@ -261,6 +283,9 @@ sub add
 			return (HTTP_TEMPORARY_REDIRECT,'limits');
 		}
 
+		# Grab friendly name
+		my $friendlyName = $params->{'inputFriendlyName'};
+
 		# Check POST data
 		my $username;
 		if (!defined($username = isUsername($params->{'inputUsername'}))) {
@@ -270,36 +295,80 @@ sub add
 		if (!defined($ipAddress = isIP($params->{'inputIP'}))) {
 			push(@errors,"IP address is not valid");
 		}
-		my $trafficLimitTx;
-		if (!defined($trafficLimitTx = isNumber($params->{'inputLimitTx'}))) {
-			push(@errors,"Download limit is not valid");
+		my $trafficClass;
+		if (!defined($trafficClass = isTrafficClassValid($params->{'inputTrafficClass'}))) {
+			push(@errors,"Traffic class is not valid");
 		}
-		my $trafficLimitRx;
-		if (!defined($trafficLimitRx = isNumber($params->{'inputLimitRx'}))) {
-			push(@errors,"Upload limit is not valid");
+		my $trafficLimitTx = isNumber($params->{'inputLimitTx'});
+		my $trafficLimitTxBurst = isNumber($params->{'inputLimitTxBurst'});
+		if (!defined($trafficLimitTx) && !defined($trafficLimitTxBurst)) {
+			push(@errors,"A valid download CIR and/or limit is required");
 		}
+		my $trafficLimitRx = isNumber($params->{'inputLimitRx'});
+		my $trafficLimitRxBurst = isNumber($params->{'inputLimitRxBurst'});
+		if (!defined($trafficLimitRx) && !defined($trafficLimitRxBurst)) {
+			push(@errors,"A valid upload CIR and/or limit is required");
+		}
+
+		my $expires = 0;
+		if (defined($params->{'inputExpires'}) && $params->{'inputExpires'} ne "") {
+			if (!defined($expires = isNumber($params->{'inputExpires'}))) {
+				push(@errors,"Expires value is not valid");
+			# Check the modifier
+			} else {
+				# Check if its defined
+				if (defined($params->{'inputExpiresModifier'}) && $params->{'inputExpiresModifier'} ne "") {
+					# Minutes
+					if ($params->{'inputExpiresModifier'} eq "m") {
+						$expires *= 60;
+					# Hours
+					} elsif ($params->{'inputExpiresModifier'} eq "h") {
+						$expires *= 3600;
+					# Days
+					} elsif ($params->{'inputExpiresModifier'} eq "d") {
+						$expires *= 86400;
+					} else {
+						push(@errors,"Expires modifier is not valid");
+					}
+				}
+				# Set right time for expiry
+				$expires += time();
+			}
+		}
+		# Grab notes
+		my $notes = $params->{'inputNotes'};
 
 		# If there are no errors we need to push this update
 		if (!@errors) {
 			# Build limit
 			my $limit = {
+				'FriendlyName' => $friendlyName,
 				'Username' => $username,
 				'IP' => $ipAddress,
 				'GroupID' => 1,
-				'ClassID' => 1,
+				'ClassID' => $trafficClass,
 				'TrafficLimitTx' => $trafficLimitTx,
+				'TrafficLimitTxBurst' => $trafficLimitTxBurst,
 				'TrafficLimitRx' => $trafficLimitRx,
-				'TrafficLimitTxBurst' => $trafficLimitTx,
-				'TrafficLimitRxBurst' => $trafficLimitRx,
-				'Status' => "online",
+				'TrafficLimitRxBurst' => $trafficLimitRxBurst,
+				'Expires' => $expires,
+				'Notes' => $notes,
 				'Source' => "plugin.webserver.limits",
 			};
 
 			# Throw the change at the config manager
-			$kernel->post("configmanager" => "process_change" => $limit);
+			$kernel->post("configmanager" => "process_limit_change" => $limit);
 
-			$logger->log(LOG_INFO,"[WEBSERVER/LIMIS/ADD] User: $username, IP: $ipAddress, Group: 1, Class: 2, ".
-					"Limits: ".prettyUndef($trafficLimitTx)."/".prettyUndef($trafficLimitRx).", Burst: ".prettyUndef($trafficLimitTx)."/".prettyUndef($trafficLimitRx));
+			$logger->log(LOG_INFO,'[WEBSERVER/LIMITS/ADD] User: %s, IP: %s, Group: %s, Class: %s, Limits: %s/%s, Burst: %s/%s',
+					prettyUndef($username),
+					prettyUndef($ipAddress),
+					undef,
+					prettyUndef($trafficClass),
+					prettyUndef($trafficLimitTx),
+					prettyUndef($trafficLimitRx),
+					prettyUndef($trafficLimitTxBurst),
+					prettyUndef($trafficLimitRxBurst)
+			);
 
 			return (HTTP_TEMPORARY_REDIRECT,'limits');
 		}
@@ -322,32 +391,42 @@ EOF
 	# Spit out errors if we have any
 	if (@errors > 0) {
 		foreach my $error (@errors) {
-			$content .= '<div class="alert alert-error">'.$error.'</div>';
+			$content .= '<div class="alert alert-danger">'.$error.'</div>';
 		}
 	}
 
 	# Generate traffic class list
 	my $trafficClasses = getTrafficClasses();
 	my $trafficClassStr = "";
-	foreach my $classID (keys %{$trafficClasses}) {
+	foreach my $classID (sort keys %{$trafficClasses}) {
 		$trafficClassStr .= '<option value="'.$classID.'">'.$trafficClasses->{$classID}.'</option>';
 	}
 
 	# Header
 	$content .=<<EOF;
 	<div class="form-group">
+		<label for="inputFriendlyName" class="col-lg-2 control-label">Friendly Name</label>
+		<div class="row">
+			<div class="col-lg-4 input-group">
+				<input name="inputFriendlyName" type="text" placeholder="Opt. Friendly Name" class="form-control" value="$params->{'inputFriendlyName'}" />
+			</div>
+		</div>
+	</div>
+	<div class="form-group">
 		<label for="inputUsername" class="col-lg-2 control-label">Username</label>
 		<div class="row">
-			<div class="col-lg-4">
+			<div class="col-lg-4 input-group">
 				<input name="inputUsername" type="text" placeholder="Username" class="form-control" value="$params->{'inputUsername'}" />
+				<span class="input-group-addon">*</span>
 			</div>
 		</div>
 	</div>
 	<div class="form-group">
 		<label for="inputIP" class="col-lg-2 control-label">IP Address</label>
 		<div class="row">
-			<div class="col-lg-4">
+			<div class="col-lg-4 input-group">
 				<input name="inputIP" type="text" placeholder="IP Address" class="form-control" value="$params->{'inputIP'}" />
+				<span class="input-group-addon">*</span>
 			</div>
 		</div>
 	</div>
@@ -365,7 +444,7 @@ EOF
 		<label for="inputExpires" class="col-lg-2 control-label">Expires</label>
 		<div class="row">
 			<div class="col-lg-2">
-				<input name="inputExpires" type="text" placeholder="Expires" class="form-control" value="$params->{'inputExpires'}" />
+				<input name="inputExpires" type="text" placeholder="Opt. Expires" class="form-control" value="$params->{'inputExpires'}" />
 			</div>
 			<div class="col-lg-2">
 				<select name="inputExpiresModifier" placeholder="Expires Modifier" class="form-control" value="$params->{'inputExpiresModifier'}">
@@ -377,32 +456,49 @@ EOF
 		</div>
 	</div>
 	<div class="form-group">
-		<label for="inputLimitTx" class="col-lg-2 control-label">Download Limit</label>
+		<label for="inputLimitTx" class="col-lg-2 control-label">Download CIR</label>
 		<div class="row">
 			<div class="col-lg-3">
 				<div class="input-group">
-					<input name="inputLimitTx" type="text" placeholder="TX Limit" class="form-control" value="$params->{'inputLimitTx'}" />
+					<input name="inputLimitTx" type="text" placeholder="Download CIR" class="form-control" value="$params->{'inputLimitTx'}" />
+					<span class="input-group-addon">Kbps *<span>
+				</div>
+			</div>
+
+			<label for="inputLimitTxBurst" class="col-lg-1 control-label">Limit</label>
+			<div class="col-lg-3">
+				<div class="input-group">
+					<input name="inputLimitTxBurst" type="text" placeholder="Download Limit" class="form-control" value="$params->{'inputLimitTxBurst'}" />
+					<span class="input-group-addon">Kbps<span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label for="inputLimitRx" class="col-lg-2 control-label">Upload CIR</label>
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="input-group">
+					<input name="inputLimitRx" type="text" placeholder="Upload CIR" class="form-control" value="$params->{'inputLimitRx'}" />
+					<span class="input-group-addon">Kbps *<span>
+				</div>
+			</div>
+
+			<label for="inputLimitRxBurst" class="col-lg-1 control-label">Limit</label>
+			<div class="col-lg-3">
+				<div class="input-group">
+					<input name="inputLimitRxBurst" type="text" placeholder="Upload Limit" class="form-control" value="$params->{'inputLimitRxBurst'}" />
 					<span class="input-group-addon">Kbps<span>
 				</div>
 			</div>
 		</div>
 	</div>
 	<div class="form-group">
-		<label for="inputLimitRx" class="col-lg-2 control-label">Upload Limit</label>
-		<div class="row">
-			<div class="col-lg-3">
-				<div class="input-group">
-					<input name="inputLimitRx" type="text" placeholder="RX Limit" class="form-control" value="$params->{'inputLimitRx'}" />
-					<span class="input-group-addon">Kbps<span>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="inputDescription" class="col-lg-2 control-label">Description</label>
+		<label for="inputNotes" class="col-lg-2 control-label">Notes</label>
 		<div class="row">
 			<div class="col-lg-4">
-				<textarea name="inputDescription" placeholder="Description" rows="3" class="form-control"></textarea>
+				<textarea name="inputNotes" placeholder="Opt. Notes" rows="3" class="form-control"></textarea>
 			</div>
 		</div>
 	</div>
@@ -418,3 +514,4 @@ EOF
 
 
 1;
+# vim: ts=4
