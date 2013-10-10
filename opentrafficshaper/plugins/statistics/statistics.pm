4395870383acba09339311be4e5496a46ae27f9f
Nigel Kukard <nkukard@lbsd.net>
2013-10-10 19:44:42 +0000

    Major code refactoring, cleanup and optimization
    The only supported DB at this time is MySQL/MariaDB

--- opentrafficshaper/plugins/statistics/statistics.pm
+++ opentrafficshaper/plugins/statistics/statistics.pm
@@ -39,6 +39,8 @@ require Exporter;
 our (@ISA,@EXPORT,@EXPORT_OK);
 @ISA = qw(Exporter);
 @EXPORT = qw(
+	STATISTICS_PERIOD
+
 	STATISTICS_DIR_TX
 	STATISTICS_DIR_RX
 );
@@ -50,7 +52,9 @@ our (@ISA,@EXPORT,@EXPORT_OK);
 use constant {
 	VERSION => '0.0.1',
 	# How often our config check ticks
-	TICK_PERIOD => 1,
+	TICK_PERIOD => 5,
+
+	STATISTICS_PERIOD => 60,
 
 	STATISTICS_DIR_TX => 1,
 	STATISTICS_DIR_RX => 2,
@@ -103,12 +107,11 @@ my $statsConfig = {
 
 
 # Handle of DBI
-my $dbi;
+my $dbh;
 # DB user mappings
 my $statsDBLIDMap = { };
 # Stats queue
 my $statsQueue = [ ];
-my $cleanupQueue = [ ];
 # Stats ubscribers
 my $subscribers;
 # Prepared statements we need...
@@ -169,7 +172,7 @@ sub plugin_init
 
 	# Create DBI agent
 	if (defined($config->{'db_dsn'})) {
-		$dbi = DBI->connect(
+		$dbh = DBI->connect(
 				$config->{'db_dsn'}, $config->{'db_username'}, $config->{'db_password'},
 				{
 					'AutoCommit' => 1,
@@ -177,54 +180,66 @@ sub plugin_init
 					'FetchHashKeyName' => 'NAME_lc'
 				}
 		);
-		if (!defined($dbi)) {
-# FIXME
-#warn $DBI::errstr;
+		if (!defined($dbh)) {
+			$logger->log(LOG_ERR,"[STATISTICS] Failed to connect to database: %s",$DBI::errstr);
 		}
 
-		# If we're working with SQLite, we need to apply some performance pragma's
-		if ($config->{'db_dsn'} =~ /sqlite/i) {
-			$logger->log(LOG_INFO,"[STATISTICS] Applied SQLite PRAGMA's");
-			$dbi->do("PRAGMA journal_mode = OFF");
-			$dbi->do("PRAGMA synchronous = OFF");
-			$dbi->do("PRAGMA cache_size = -32768");
-		}
-
-		my $res = $dbi->prepare('
-			INSERT INTO users (`Username`) VALUES (?)
-		');
-		if ($res) {
+		# Prepare user add statement
+		if ($dbh && (my $res = $dbh->prepare('INSERT INTO users (`Username`) VALUES (?)'))) {
 			$statsPreparedStatements->{'user_add'} = $res;
+		} else {
+			$logger->log(LOG_ERR,"[STATISTICS] Failed to prepare statement 'user_add': %s",$DBI::errstr);
+			$dbh->disconnect();
+			$dbh = undef;
 		}
-#FIXME
 
-		$statsPreparedStatements->{'user_get'} = $dbi->prepare('
-			SELECT ID FROM users WHERE `Username` = ?
-		');
-# FIXME
-		$statsPreparedStatements->{'stats_add'} = $dbi->prepare('
-			INSERT INTO stats
-				(
-					`UserID`, `Key`, `Timestamp`,
-					`Direction`,
-					`CIR`, `Limit`, `Rate`, `PPS`, `Queue_Len`,
-					`Total_Bytes`, `Total_Packets`, `Total_Overlimits`, `Total_Dropped`
-				)
-			VALUES
-				(
-					?, ?, ?,
-					?,
-					?, ?, ?, ?, ?,
-					?, ?, ?, ?
-				)
-		');
+		# Prepare user get statement
+		if ($dbh && (my $res = $dbh->prepare('SELECT ID FROM users WHERE `Username` = ?'))) {
+			$statsPreparedStatements->{'user_get'} = $res;
+		} else {
+			$logger->log(LOG_ERR,"[STATISTICS] Failed to prepare statement 'user_get': %s",$DBI::errstr);
+			$dbh->disconnect();
+			$dbh = undef;
+		}
+
+		# Prepare stats consolidation statement
+		if ($dbh && (my $res = $dbh->prepare('
+			SELECT
+				`UserID`, `Timestamp` - (`Timestamp` % ?) AS TimestampM,
+				`Direction`,
+				MAX(`CIR`) AS `CIR`, MAX(`Limit`) AS `Limit`, MAX(`Rate`) AS `Rate`, MAX(`PPS`) AS `PPS`, MAX(`Queue_Len`) AS `Queue_Len`,
+				AVG(`Total_Bytes`) AS `Total_Bytes`, AVG(`Total_Packets`) AS `Total_Packets`, AVG(`Total_Overlimits`) AS `Total_Overlimits`, AVG(`Total_Dropped`) AS `Total_Dropped`
+			FROM
+				stats
+			WHERE
+				`Key` = ?
+				AND `Timestamp` < ?
+			GROUP BY
+				`UserID`, `TimestampM`, `Direction`
+		'))) {
+			$statsPreparedStatements->{'stats_consolidate'} = $res;
+		} else {
+			$logger->log(LOG_ERR,"[STATISTICS] Failed to prepare statement 'stats_consolidate': %s",$DBI::errstr);
+			$dbh->disconnect();
+			$dbh = undef;
+		}
+
+		# Prepare stats cleanup statement
+		if ($dbh && (my $res = $dbh->prepare('DELETE FROM stats WHERE `Key` = ? AND `Timestamp` < ?'))) {
+			$statsPreparedStatements->{'stats_cleanup'} = $res;
+		} else {
+			$logger->log(LOG_ERR,"[STATISTICS] Failed to prepare statement 'stats_cleanup': %s",$DBI::errstr);
+			$dbh->disconnect();
+			$dbh = undef;
+		}
 
 		# Set last cleanup to now
-		foreach my $key (keys %{$statsConfig}) {
-			$lastCleanup->{$key} = time();
+		if ($dbh) {
+			foreach my $key (keys %{$statsConfig}) {
+				$lastCleanup->{$key} = time();
+			}
 		}
 	}
-
 
 	return 1;
 }
@@ -265,7 +280,7 @@ sub session_stop
 
 	# Tear down data
 	$globals = undef;
-	$dbi = undef;
+	$dbh = undef;
 	$statsDBLIDMap = { };
 	$subscribers = undef;
 	$statsPreparedStatements = { };
@@ -282,43 +297,41 @@ sub session_tick
 {
 	my ($kernel,$heap) = @_[KERNEL,HEAP];
 
+
+	# If we don't have a DB handle, just skip...
+	if (!$dbh) {
+		return;
+	}
+
+	# Pull in statements
+	my $sthStatsConsolidate = $statsPreparedStatements->{'stats_consolidate'};
+	my $sthStatsCleanup = $statsPreparedStatements->{'stats_cleanup'};
+
 	my $now = time();
 	my $timer1 = [gettimeofday];
 
-	my $statsAddSTH = $statsPreparedStatements->{'stats_add'};
-
-	my $maxFlush = int(@{$statsQueue} / 300) + 1;
+	# Even out flushing over 10s to absorb spikes
+	my $maxFlush = int(@{$statsQueue} / 10) + 100;
 	my $numFlush = 0;
 
-	# Loop...
-	my @tstSt;
-	my @tstVals;
+	# Loop and build the data to create our multi-insert
+	my @insertHolders;
+	my @insertData;
 	while (defined(my $stat = shift(@{$statsQueue})) && $numFlush < $maxFlush) {
-		# NK
-#		my $res = $statsAddSTH->execute(
-#			$stat->{'userid'}, $stat->{'key'}, $stat->{'timestamp'},
-#			$stat->{'direction'},
-#			$stat->{'cir'}, $stat->{'limit'}, $stat->{'rate'}, $stat->{'pps'}, $stat->{'queue_len'},
-#			$stat->{'total_bytes'}, $stat->{'total_packets'}, $stat->{'total_overlimits'}, $stat->{'total_dropped'}
-#		);
-#		if (!defined($res)) {
-# FIXME
-#warn "DB QUERY FAILED: ".$statsAddSTH->errstr();
-#		}
-		push(@tstVals,
+		push(@insertData,
 			$stat->{'userid'}, $stat->{'key'}, $stat->{'timestamp'},
 			$stat->{'direction'},
 			$stat->{'cir'}, $stat->{'limit'}, $stat->{'rate'}, $stat->{'pps'}, $stat->{'queue_len'},
 			$stat->{'total_bytes'}, $stat->{'total_packets'}, $stat->{'total_overlimits'}, $stat->{'total_dropped'}
 		);
-		push(@tstSt,"(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		push(@insertHolders,"(?,?,?,?,?,?,?,?,?,?,?,?,?)");
 		$numFlush++;
 	}
 
-# FIXME - TEST
-	if (@tstVals > 0) {
-	my $res = $dbi->do('
-			INSERT INTO stats
+	# If we got things to insert, do it
+	if (@insertHolders > 0) {
+		my $res = $dbh->do('
+			INSERT DELAYED INTO stats
 				(
 					`UserID`, `Key`, `Timestamp`,
 					`Direction`,
@@ -326,63 +339,24 @@ sub session_tick
 					`Total_Bytes`, `Total_Packets`, `Total_Overlimits`, `Total_Dropped`
 				)
 			VALUES
-	'.join(',',@tstSt),undef,@tstVals);
-
+				'.join(',',@insertHolders),undef,@insertData
+		);
+		# Check for error
 		if (!defined($res)) {
-#warn "DB QUERY FAILED: ".$dbi->errstr();
+			$logger->log(LOG_ERR,"[STATISTICS] Failed to execute delayed stats insert: %s",$DBI::errstr);
 		}
 	}
 
 	my $timer2 = [gettimeofday];
+# FIXME
+if ($numFlush) {
 	my $timediff2 = tv_interval($timer1,$timer2);
-	warn "STATS FLUSH: $numFlush/$maxFlush records, time ".sprintf('%.3fs',$timediff2);
-
-
-
-
+	$logger->log(LOG_INFO,"[STATISTICS] Stats flush time %s/%s records: %s",$numFlush,$maxFlush,sprintf('%.3fs',$timediff2));
+}
 
 	my $res;
 
 
-	my $sth = $dbi->prepare('
-		SELECT
-			*
-		FROM
-			stats
-		WHERE
-			`Key` = ?
-			AND `Timestamp` > ?
-			AND `Timestamp` < ?
-	');
-
-	my $sth2 = $dbi->prepare('
-		DELETE
-		FROM
-			stats
-		WHERE
-			`Key` = ?
-			AND `Timestamp` < ?
-	');
-
-	my $sth3 = $dbi->prepare('
-		SELECT
-			ID
-		FROM
-			stats
-		WHERE
-			`Key` = ?
-			AND `Timestamp` < ?
-	');
-
-
-
-	my @statsMaxItems = (
-		'cir','limit','rate','pps','queue_len',
-	);
-
-	my @statsAvgItems = (
-		'total_bytes','total_packets','total_overlimits','total_dropped'
-	);
 
 	# Loop with our stats consolidation configuration
 	foreach my $key (keys %{$statsConfig}) {
@@ -400,93 +374,63 @@ sub session_tick
 		# Set last cleanup to now
 		$lastCleanup->{$key} = $now;
 
-		# Pull last periods rows
-		$res = $sth->execute($prevKey,$lastPeriod - $precision,$lastPeriod);
+		# Execute and pull in consolidated stats
+		$res = $sthStatsConsolidate->execute($prevKey,$precision,$lastPeriod - $precision);
+		if ($res) {
+			# Loop with items returned
+			while (my $item = $sthStatsConsolidate->fetchrow_hashref()) {
+				$item->{'key'} = $key;
+				$item->{'timestamp'} = $item->{'timestampm'};
 
-		my $cData = {};
-		while (my $item = $sth->fetchrow_hashref()) {
-			my $itemPeriod = _getAlignedTime($item->{'timestamp'},$precision);
-
-			# If this user for this period or direction doesn't exist, create it
-			if (
-					!defined($cData->{$item->{'userid'}}) ||
-					!defined($cData->{$item->{'userid'}}->{$itemPeriod}) ||
-					!defined($cData->{$item->{'userid'}}->{$itemPeriod}->{$item->{'direction'}})
-			) {
-				# Set 0's for everything
-				foreach my $key (@statsMaxItems,@statsAvgItems) {
-					$cData->{$item->{'userid'}}->{$itemPeriod}->{$item->{'direction'}}->{$key} = 0;
-				}
-				# And number of records to 0 too
-				$cData->{$item->{'userid'}}->{$itemPeriod}->{$item->{'direction'}}->{'_recs'} = 0;
+				# Queue for insert
+				push(@{$statsQueue},$item);
 			}
-
-			# Setup easy to use stats variable
-			my $curStat = $cData->{$item->{'userid'}}->{$itemPeriod}->{$item->{direction}};
-
-			# Check max items
-			foreach my $key (@statsMaxItems) {
-					# Only set if current item is higher
-					if ($item->{$key} > $curStat->{$key}) {
-						$curStat->{$key} = $item->{$key};
-					}
-			}
-			# Work out averages
-			foreach my $key (@statsAvgItems) {
-					$curStat->{$key} += $item->{$key};
-			}
-
-			$curStat->{'_recs'}++;
-		}
-
-		# Loop with user ID's
-		foreach my $userid (keys %{$cData}) {
-			# Loop with periods
-			foreach my $period (keys %{$cData->{$userid}}) {
-				# Loop with directions
-				foreach my $direction (keys %{$cData->{$userid}->{$period}}) {
-					my $curStat = $cData->{$userid}->{$period}->{$direction};
-					# Make sure our avrages are divided properly
-					foreach my $item (@statsAvgItems) {
-						$curStat->{$item} = int($curStat->{$item} / $curStat->{'_recs'});
-					}
-
-					# Setup other items we need to insert
-					$curStat->{'userid'} = $userid;
-					$curStat->{'key'} = $key;
-					$curStat->{'timestamp'} = $period;
-					$curStat->{'direction'} = $direction;
-
-					# Queue for insert
-					push(@{$statsQueue},$curStat);
-				}
-			}
+		# If there was an error, make sure we report it
+		} else {
+			$logger->log(LOG_ERR,"[STATISTICS] Failed to execute stats consolidation statement: %s",$sthStatsConsolidate->errstr());
 		}
 
 		my $timerB = [gettimeofday];
 		my $timediffB = tv_interval($timerA,$timerB);
-		warn "COMPUTE TIME: $key @ ".sprintf('%.3fs',$timediffB);
+		$logger->log(LOG_INFO,"[STATISTICS] Stats consolidation time for key %s: %s",$key,sprintf('%.3fs',$timediffB));
 	}
 
+	# Setup another timer
 	my $timer3 = [gettimeofday];
 
-# FIXME hard coded 1800 for the stats stream
-	$res = $sth2->execute(0, $now - 1800);
-
+# FIXME
+if (!defined($lastCleanup->{'0'}) || $lastCleanup->{'0'} + 300 < $now) {
+warn "CLEANUP";
+	# Streamed stats is hard coded to 900s
+	$res = $sthStatsCleanup->execute(0, $now - 900);
+	if ($res) {
+		# We get 0E0 for 0 when none were removed
+		$res = 0 if ($res eq "0E0");
+		$logger->log(LOG_INFO,"[STATISTICS] Cleanup streamed stats %s",$res);
+	} else {
+		$logger->log(LOG_ERR,"[STATISTICS] Failed to execute stats cleanup statement: %s",$sthStatsCleanup->errstr());
+	}
+	# Loop and remove retained stats
 	foreach my $key (keys %{$statsConfig}) {
-		$res = $sth2->execute($key, $now - ($statsConfig->{$key}->{'retention'} * 86400)  );
+		$res = $sthStatsCleanup->execute($key, $now - ($statsConfig->{$key}->{'retention'} * 86400));
+		if ($res) {
+			# We get 0E0 for 0 when none were removed
+			$res = 0 if ($res eq "0E0");
+			$logger->log(LOG_INFO,"[STATISTICS] Cleanup key %s stats %s",$key,$res);
+		} else {
+			$logger->log(LOG_ERR,"[STATISTICS] Failed to execute stats cleanup statement for key %s: %s",$key,$sthStatsCleanup->errstr());
+		}
 	}
 
-
+	$lastCleanup->{'0'} = $now;
 
 	my $timer4 = [gettimeofday];
 	my $timediff4 = tv_interval($timer3,$timer4);
-	 warn "DB CLEAN TIME: ".sprintf('%.3fs',$timediff4);
+	$logger->log(LOG_INFO,"[STATISTICS] Stats cleanup time: %s",sprintf('%.3fs',$timediff4));
+
+}
 
 
-
-
-END:
 	# Set delay on config updates
 	$kernel->delay(tick => TICK_PERIOD);
 }
@@ -502,15 +446,14 @@ sub do_update
 	my ($kernel, $statsData) = @_[KERNEL, ARG0];
 
 
-
-
-my $timer0 = [gettimeofday];
+	# FIXME? This requires DB access
+	if (!$dbh) {
+		return;
+	}
 
 	# Loop through stats data we got
 	foreach my $rawItem (keys %{$statsData}) {
 		my $stat = $statsData->{$rawItem};
-
-
 		# ID of the stat item in the stats table
 		my $statsID;
 
@@ -532,8 +475,6 @@ warn "IT BLEW UP!!!";
 
 		push(@{$statsQueue},$stat);
 
-
-
 #		# Check if we have an event handler subscriber for this item
 #		if (defined($subscribers->{$statsItem}) && %{$subscribers->{$statsItem}}) {
 #			# If we do, loop with them
@@ -553,12 +494,6 @@ warn "IT BLEW UP!!!";
 #		}
 	}
 
-	my $timer1 = [gettimeofday];
-	 my $timediff1 = tv_interval($timer0,$timer1);
-	 warn "STATS TIME: ".sprintf('%.3fs',$timediff1);
-
-#	use Devel::Size qw(total_size);
-#	warn "QUEUE SIZE: ".total_size($statsQueue);
 }
 
 
@@ -629,7 +564,7 @@ sub getStats
 	# Grab stats ID from LID
 	my $userid = _getCachedStatsIDFromLID($lid);
 
-	my $sth = $dbi->prepare('
+	my $sth = $dbh->prepare('
 		SELECT
 			`Timestamp`, `Direction`, `Rate`, `PPS`, `CIR`, `Limit`
 		FROM
@@ -745,7 +680,7 @@ warn "FAILED TO EXECUTE GETUSER: ".$userGetSTH->errstr;
 	# Try add it to the DB
 	my $userAddSTH = $statsPreparedStatements->{'user_add'};
 	if (my $res = $userAddSTH->execute($username)) {
-		return $statsDBLIDMap->{$lid} = $dbi->last_insert_id("","","","");
+		return $statsDBLIDMap->{$lid} = $dbh->last_insert_id("","","","");
 	} else {
 warn "DB ADD USER ERROR: ".$userAddSTH->errstr;
 	}
