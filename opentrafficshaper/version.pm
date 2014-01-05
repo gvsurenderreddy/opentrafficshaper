170f40e36b5e70b7b02bcbec7ba78df142b67e4b
Nigel Kukard <nkukard@lbsd.net>
2014-01-05 17:32:13 +0000

    Bumped copyright, removed spurious space

--- opentrafficshaper/version.pm
+++ opentrafficshaper/version.pm
@@ -1,6 +1,6 @@
 # OpenTrafficShaper version package
-# Copyright (C) 2007-2013, AllWorldIT
-# 
+# Copyright (C) 2007-2014, AllWorldIT
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
